718ca4e28404dc34c0a15135dfb6198d25a3d73a
Walter Stocker <walter.stocker@siemens-healthineers.com>
2018-05-30 00:09:17 +0200

    feat: blog-entries now have markdown content

--- src/components/pages/blog-entries-container.js
+++ src/components/pages/blog-entries-container.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import showdown from 'showdown'
+
 import BlogEntry from './blog-entry'
 
-const BlogEntriesContainer = ({ blogEntries }) => (
-  <div>
-    {blogEntries.map((blogEntry, index) => (
-      <BlogEntry
-        key={index}
-        title={blogEntry.title}
-        modified={blogEntry.modified}
-        author={blogEntry.author}
-      >
-        {blogEntry.content}
-      </BlogEntry>
-    ))}
-  </div>
-)
+const BlogEntriesContainer = ({ blogEntries }) => {
+  let converter = new showdown.Converter()
+
+  return (
+    <div>
+      {blogEntries.map((blogEntry, index) => (
+        <BlogEntry
+          key={index}
+          title={blogEntry.title}
+          modified={blogEntry.modified}
+          author={blogEntry.author}
+        >
+          <div
+            dangerouslySetInnerHTML={{
+              __html: converter.makeHtml(blogEntry.content)
+            }}
+          />
+        </BlogEntry>
+      ))}
+    </div>
+  )
+}
 
 BlogEntriesContainer.propTypes = {
   blogEntries: PropTypes.array.isRequired
