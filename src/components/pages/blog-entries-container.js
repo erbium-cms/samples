b7d6dfca4d2d99c2d0e4cda48bb2c632c26a8484
walter <walter.stocker@gmx.de>
2018-06-05 23:11:25 +0200

    chore: fixed false positive for eslint

--- src/components/pages/blog-entries-container.js
+++ src/components/pages/blog-entries-container.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import showdown from 'showdown'
-import prettify from 'showdown-prettify'
+import prettify from 'showdown-prettify' // eslint-disable-line no-unused-vars
 
 import BlogEntry from './blog-entry'
 
